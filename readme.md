3b25a0fdb517e3821c145442605f88af827c69f9
Leonard Teo <teo.leonard@gmail.com>
2011-09-24 11:40:23 -0400

    Added fuel

---
+++ readme.md
@@ -0,0 +1,5 @@
+# INSE 6530 Course
+
+	Professor: Nizar Bouguila, Associate Professor, PhD, PEng
+	bouguila@ciise.concordia.ca
+	Office EV-007-632 
---
+++ web/fuel/app/logs/2011/09/24.php
@@ -0,0 +1,16 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Debug - 2011-09-24 15:33:30 --> Notice - Position larger than number of elements in array in which to insert. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 258
+Debug - 2011-09-24 15:33:30 --> Notice - Unknown key after which to insert the new value into the array. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 287
+Debug - 2011-09-24 15:33:30 --> Notice - Unknown value after which to insert the new value into the array. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 313
+Debug - 2011-09-24 15:33:33 --> testing log debug
+Error - 2011-09-24 15:33:33 --> testing log error
+Debug - 2011-09-24 15:33:33 --> Log::debug - testing log debug
+Error - 2011-09-24 15:33:33 --> Log::error - testing log error
+Debug - 2011-09-24 15:37:43 --> Notice - Position larger than number of elements in array in which to insert. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 258
+Debug - 2011-09-24 15:37:43 --> Notice - Unknown key after which to insert the new value into the array. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 287
+Debug - 2011-09-24 15:37:43 --> Notice - Unknown value after which to insert the new value into the array. in /Users/leonardteo/Documents/webprojects/INSE6530/web/fuel/core/tests/arr.php on line 313
+Debug - 2011-09-24 15:37:45 --> testing log debug
+Error - 2011-09-24 15:37:45 --> testing log error
+Debug - 2011-09-24 15:37:46 --> Log::debug - testing log debug
+Error - 2011-09-24 15:37:46 --> Log::error - testing log error
